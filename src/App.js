9d4affb9d62db6b995c4c34a79d2e76d7e2a4915
Divyanshu tandon <divyanshutandon@Divyanshus-MacBook-Air.local>
2025-01-13 12:06:44 +0530

    Organized folder structure for components and pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import logo from './logo.svg';
+import './App.css';
 
-const Home = () => <div className="p-4">Welcome to Task Manager Pro!</div>;
-const Tasks = () => <div className="p-4">Here are your tasks!</div>;
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tasks" element={<Tasks />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
